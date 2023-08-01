fbd3033f210526695017d18d027092f06dc52718
hasegawa <50223611+hasegawa@users.noreply.github.com>
2023-08-01 22:30:45 +0900

    DynamoDBへblueflake ID新規登録処理を実装

--- lib/lambda/api/v1-sequence.ts
+++ lib/lambda/api/v1-sequence.ts
@@ -1,16 +1,47 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb"
+import { DynamoDBClient, GetItemCommand, PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb"
+import { getNowDate, getRandomKey, makeBlueFlakeID } from "../../common/generate-id"
 
 
 const dynamoDbClient = new DynamoDBClient({})
 
-const timestampItems = async (tableName:string) => {
-    const command = new ScanCommand({
+async function getItem(tableName:string, timeStampAndRandomKey:string) {
+    const command = new GetItemCommand({
         TableName: tableName,
+        Key: {
+            timeStampAndRandomKey:  {S: timeStampAndRandomKey}
+        }
     })
-    return (await dynamoDbClient.send(command)).Items
+    return await dynamoDbClient.send(command)
+}
+
+async function registItem(tableName:string, timeStampAndRandomKey:string, sequenceNumber:number) {
+    const command = new PutItemCommand({
+        TableName: tableName,
+        Item: {
+            timeStampAndRandomKey:  {
+                S: timeStampAndRandomKey
+            },
+            sequenceNumber: {
+                N: sequenceNumber.toString()
+            }
+        }
+    })
+    return await dynamoDbClient.send(command)
 }
 
 export const handler = async () => {
     const tableName = process.env.SEQUENCE_TABLE_NAME!
-    return await timestampItems(tableName)
+    const nowDate =  getNowDate();
+    // timestamp + randomkey(ex.0001)をパーティションキーとしている
+    const randomKey = getRandomKey()
+    const timeStampAndRandomKey = nowDate.getTime().toString() + randomKey.toString().padStart(4, "0")
+    const item = await getItem(tableName, timeStampAndRandomKey)
+    const sequenceNumber = item?.Item?.sequenceNumber;
+    if (sequenceNumber === undefined) {
+        // 新規登録
+        registItem(tableName, timeStampAndRandomKey, 1)
+        return makeBlueFlakeID(nowDate, randomKey, 1)
+    }
+
+    return 0
 }

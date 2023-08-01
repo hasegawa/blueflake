62e729c65a5d46320788d195ade81b5180ee392c
hasegawa <50223611+hasegawa@users.noreply.github.com>
2023-08-01 09:59:32 +0900

    現在時刻を取得するメソッドを追加

--- lib/common/generate-id.ts
+++ lib/common/generate-id.ts
@@ -1,4 +1,14 @@
 /**
+ * どの実行環境でもJSTの現在時刻を取得する
+ * @returns
+ */
+export function getNowDate() {
+  // 実行環境によってタイムゾーンが違う可能性があるため、どのタイムゾーンでもJSTの現在時刻を計算に使用する
+  const nowTimeStamp = Date.now() + ((new Date().getTimezoneOffset() + 540) * 60 * 1000);
+  return new Date(nowTimeStamp);
+}
+
+/**
  * Generate random key.
  * @returns
  */
@@ -14,16 +24,16 @@ export function getRandomKey() {
  *   randomKey:1
  *   sequenceNumber:1
  *   -> 0000 0000 0000 0001 0100 1001 1001 0111 0000 0000 0000 0000 0001 0000 0000 0001
- * @param nowUnixTimeStamp
+ * @param nowDate
  * @param randomKey
  * @param sequenceNumber
  * @returns
  */
-export function makeBlueFlakeID(nowUnixTimeStamp: number, randomKey: number, sequenceNumber: number) {
+export function makeBlueFlakeID(nowDate: Date, randomKey: number, sequenceNumber: number) {
   // JSTの2023/7/30 00:00:00.000UnixTimeStamp（ミリ秒まで13桁）を基準とする
   const countStartTimeStamp = 1690642800000;
   // 経過時間を2進数変換後に64桁にしてからsliceで先頭23bitの切り捨て
-  const timeStampBitString = (nowUnixTimeStamp - countStartTimeStamp).toString(2).padStart(64, "0").slice(23);
+  const timeStampBitString = (nowDate.getTime() - countStartTimeStamp).toString(2).padStart(64, "0").slice(23);
   const randomKeyBitString = randomKey.toString(2).padStart(10, "0");
   const sequenceKeyBitString = sequenceNumber.toString(2).padStart(12, "0");
 
